4b0d925f867fcf478d9158545e887c5a825029ff
John Doe <johndoe@example.com>
2025-04-01 20:01:58 +0300

    feat: minor adjustments

--- scripts/start_client.sh
+++ scripts/start_client.sh
@@ -128,7 +128,7 @@ run_command "bash $PROJECT_ROOT/scripts/start_foxglove.sh" true|| handle_error "
 run_with_sudo "bash $PROJECT_ROOT/scripts/start_dds_bridge.sh" || handle_error "Не удалось запустить start_dds_bridge.sh"
 run_command "bash $PROJECT_ROOT/scripts/start_px4.sh" true || handle_error "Не удалось запустить start_px4.sh"
 run_command "bash $PROJECT_ROOT/scripts/start_qgc.sh" || handle_error "Не удалось запустить start_qgc.sh"
-run_command "bash $PROJECT_ROOT/scripts/start_react.sh" || handle_error "Не удалось запустить start_react.sh"
+# run_command "bash $PROJECT_ROOT/scripts/start_react.sh" || handle_error "Не удалось запустить start_react.sh"
 
 # Build ROS2 modules
 echo "Building ROS2..."
